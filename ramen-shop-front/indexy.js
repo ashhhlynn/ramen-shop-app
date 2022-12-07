46573d20ce504251db6f7b3afe9b22e9e09dcd4b
ashhhlynn <ashleylynnwelch@gmail.com>
2022-12-07 17:31:01 -0600

    worked on renderCart and removefromCart method in indexjs

--- ramen-shop-front/indexy.js
+++ ramen-shop-front/indexy.js
@@ -10,7 +10,28 @@ function startProgram(){
 }
 
 function renderCart(){
+    const cartContents = document.getElementById("cart-contents")
+    let listItems = document.querySelectorAll("li")
+    for (var i = 0; i < listItems.length; i++ )
+        { cartContents.innerHTML += `` }
+    let cartTotal = document.getElementById("cart-total")
+    cartTotal.innerHTML = `Total: $${cart_total}`
+    console.log(cart_total)   
+    const removeBtns = document.querySelectorAll(".remove-button")
+    removeBtns.forEach(button => button.addEventListener("click", e => removeFromCart(e)))
+}
 
 
-
+function removeFromCart(e){
+    e.preventDefault()
+    const item = Item.all.find(item => item.id == e.target.id);
+    let new_cart_total = cart_total - item.price
+    cart_total = new_cart_total
+    const index = cart_contents.indexOf(item);
+    cart_contents.splice(index, 1)
+    console.log(cart_contents)
+    let list_item = document.getElementById(`item-${item.id}`)
+    console.log(list_item)
+    list_item.remove()
+    renderCart()
 }
