a8da42b8e1aad2d148fc1a16cd1efa93c8513f28
ashhhlynn <ashleylynnwelch@gmail.com>
2022-12-09 13:29:58 -0600

    more formatting, edited taxMath functions, broke up and edited fetch menus function, changed names, separated add to cart and remove from cart methods into dom and consant object methods for both

--- ramen-shop-back/db/seeds.rb
+++ ramen-shop-back/db/seeds.rb
@@ -6,20 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ramen_menu = Menu.create(category: "ramen")
-appetizer_menu  = Menu.create(category: "appetizer")
 
-pork_gyoza = MenuItem.create(name: "Pork Gyoza", description: "Handmade pork pot stickers. 6 pieces", menu_id: 6, price: 6, image_url: "https://olo-images-live.imgix.net/42/4239cb8492e74aa1a5fa506435170073.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=b8167bc5046d50c2933b8746d5a9b33f")
 
-edemame = MenuItem.create(name: "Edemame", description: "Lightly salted boiled soybeans", menu_id: 6, price: 5, image_url: "https://olo-images-live.imgix.net/1a/1acd93ed5a1747d09a23e5278dd97a39.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=d5a895fad67bd22e226322ef9dbd3345")
+salad_menu = Menu.create(category: "salad")
 
-chicken_ramen = MenuItem.create(name: "Chicken Ramen", 
-description: "Chicken broth: chicken chashu, spinach, green onion and fried onion",
-menu_id: 5,
-price: 15,
-image_url: "https://olo-images-live.imgix.net/5b/5bc15bf2e4e9463fbd74a3db320733bb.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=08a276b28f557c079a415e41f4415084")
+house_salad = MenuItem.create(name: "House Salad", description: "Baby arugula, kale and baby mix green with Japanese dressing", menu_id: 7, price: 5, image_url: "https://olo-images-live.imgix.net/69/69bfeb3290e94b71af69eabfec7ab000.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=3cd5a9195866ca072951b124ef39a72b")
+quinoa_salad = MenuItem.create(name: "Quinoa Salad", description: "Baby greens, kale, broccoli, white quinoa, kidney beans and garbanzo beans tossed with sesame dressing, garnished with corn and cherry tomatoes", menu_id: 7, price: 8, image_url: "https://olo-images-live.imgix.net/54/54cad345c10949728be5dedfc250d9a7.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=9c588ce57ae2dd90c0e10fa54c5199f3")
 
-tonkatsu_black_ramen = MenuItem.create(name: "Tonkatsu Black", description: "Pork broth: pork chashu, kikurage, green onion, nori dried seaweed, seasoned egg*, garlic chips, garlic oil, fried onion and spicy sauce", menu_id: 5, price: 16, image_url: "https://olo-images-live.imgix.net/fd/fdba305cbfa54e41bd480a86df60b97a.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=863e2bcf36974252ce966b4639373e62")
-
-yuzu_shio_ramen = MenuItem.create(name: "Yuzu Shio", description: "Chicken & Pork clear broth: Pork chashu, green onion, spinach, seasoned egg*, nori seaweed with yuzu flavor", menu_id: 5, price: 15, image_url: "https://olo-images-live.imgix.net/26/26f16d06012d4c6db5b8a3c5dd03ee6a.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=82008a97bcd7b701495969f716500ff9")
 
